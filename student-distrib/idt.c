4e6c4937968824ddc8da449c0e96e2706bd7737e
unknown <fengzex2@illinois.edu>
2018-03-10 20:30:34 -0600

    handler

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -1,61 +1,30 @@
 #include "idt.h"
 #include "x86_desc.h"
 
-#define DE      0       /*Divide Error Exception*/
-#define DB      1       /*Debug Exception*/
-#define NMI     2       /*NMI Interrupt*/
-#define BP      3       /*Breakpoint Exception*/
-#define OF      4       /*Overflow Exception*/
-#define BR      5       /*BOUND Range Exceeded Exception*/
-#define UD      6       /*Invalid Opcode Exception*/
-#define NM      7       /*Device Not Available Exception*/
-#define DF      8       /*Double Fault Exception*/
-#define CSO     9       /*Coprocessor Segment Overrun*/
-#define TS      10      /*Invalid TSS Exception*/
-#define NP      11      /*Segment Not Present*/
-#define SS      12      /*Stack Fault Exception*/
-#define GP      13      /*General Protection Exception*/
-#define PF      14      /*Page-Fault Exception*/
-#define MF      16      /*x87 FPU Floating-Point Error*/
-#define AC      17      /*Alignment Check Exception*/
-#define MC      18      /*Machine-Check Exception*/
-#define XF      19      /*SIMD Floating-Point Exception*/
-#define KB_INT  0x21    /*keyboard*/
-#define RTC_INT 0x28    /*real time clock*/
-#define SCV     0x80    /*system call vector*/
-#define INT_GATE    0   /*interrupt gate descriptor*/
-#define TRAP_GATE   1   /*trap gata descriptor*/
-#define CALL_GATE   2   /*call gate descriptor*/
-#define DPL_IE      0   /*Attempting  to  access  an  interrupt  or  exception
-                        handler  through  an  interrupt  or  trap  gate from
-                        virtual-8086 mode when the handler’s code segment DPL
-                        is greater than 0.*/
-#define DPL_SC      3   /*dpl of system call*/
-                        /*Gate descriptors are in themselves “gates,” which
-                        hold pointers to procedure entry points in code segments
-                        (call, interrupt, and trap gates) */
-idt_init(DE, TRAP_GATE);
-idt_init(DB, TRAP_GATE);
-idt_init(NMI, TRAP_GATE);
-idt_init(BP, TRAP_GATE);
-idt_init(OF, TRAP_GATE);
-idt_init(BR, TRAP_GATE);
-idt_init(UD, TRAP_GATE);
-idt_init(NM, TRAP_GATE);
-idt_init(CSO, TRAP_GATE);
-idt_init(TS, TRAP_GATE);
-idt_init(NP, TRAP_GATE);
-idt_init(SS, TRAP_GATE);
-idt_init(GP, TRAP_GATE);
-idt_init(PF, TRAP_GATE);
-idt_init(MF, TRAP_GATE);
-idt_init(AC, TRAP_GATE);
-idt_init(MC, TRAP_GATE);
-idt_init(XF, TRAP_GATE);
+idt_init(DE, TRAP_GATE, exception_DE);
+idt_init(DB, TRAP_GATE, exception_DB);
+idt_init(NMI, TRAP_GATE, exception_NMI);
+idt_init(BP, TRAP_GATE, exception_BP);
+idt_init(OF, TRAP_GATE, exception_OF);
+idt_init(BR, TRAP_GATE, exception_BR);
+idt_init(UD, TRAP_GATE, exception_UD);
+idt_init(NM, TRAP_GATE, exception_NM);
+idt_init(CSO, TRAP_GATE, exception_CSO);
+idt_init(TS, TRAP_GATE, exception_TS);
+idt_init(NP, TRAP_GATE, exception_NP);
+idt_init(SS, TRAP_GATE, exception_SS);
+idt_init(GP, TRAP_GATE, exception_GP);
+idt_init(PF, TRAP_GATE, exception_PF);
+idt_init(MF, TRAP_GATE, exception_MF);
+idt_init(AC, TRAP_GATE, exception_AC);
+idt_init(MC, TRAP_GATE, exception_MC);
+idt_init(XF, TRAP_GATE, exception_XF);
+
 /*IA-32 5.11 & 4.8*/
-void idt_init(unsigned index, unsigned gateType) {
+void idt_init(unsigned index, unsigned gateType, uint32_t handler) {
     idt_desc_t temp = idt[index];
     desc.seg_selector = KERNEL_CS;
+    SET_IDT_ENTRY(temp, handler);
     desc.reserved4 = 0;
     desc.size = 1;
     desc.present = 1;
@@ -83,4 +52,118 @@ void idt_init(unsigned index, unsigned gateType) {
             }
     }
     idt[index] = temp;
+}
+
+void exception_DE() {
+    cli();
+    printf("Divide Error Exception\n");
+    while(1);
+}
+
+void exception_DB() {
+    cli();
+    printf("Debug Exception\n");
+    while(1);
+}
+
+void exception_NMI() {
+    cli();
+    printf("NMI Interrupt\n");
+    while(1);
+}
+
+void exception_BP() {
+    cli();
+    printf("Breakpoint Exception\n");
+    while(1);
+}
+
+void exception_OF() {
+    cli();
+    printf("Overflow Exception\n");
+    while(1);
+}
+
+void exception_BR() {
+    cli();
+    printf("BOUND Range Exceeded Exception\n");
+    while(1);
+}
+
+void exception_UD() {
+    cli();
+    printf("Invalid Opcode Exception\n");
+    while(1);
+}
+
+void exception_NM() {
+    cli();
+    printf("Device Not Available Exception\n");
+    while(1);
+}
+
+void exception_DF() {
+    cli();
+    printf("Double Fault Exception\n");
+    while(1);
+}
+
+void exception_CSO() {
+    cli();
+    printf("Coprocessor Segment Overrun\n");
+    while(1);
+}
+
+void exception_TS() {
+    cli();
+    printf("Invalid TSS Exception\n");
+    while(1);
+}
+
+void exception_NP() {
+    cli();
+    printf("Segment Not Present\n");
+    while(1);
+}
+
+void exception_SS() {
+    cli();
+    printf("Stack Fault Exception\n");
+    while(1);
+}
+
+void exception_GP() {
+    cli();
+    printf("General Protection Exception\n");
+    while(1);
+}
+
+void exception_PF() {
+    cli();
+    printf("Page-Fault Exception\n");
+    while(1);
+}
+
+void exception_MF() {
+    cli();
+    printf("x87 FPU Floating-Point Error\n");
+    while(1);
+}
+
+void exception_AC() {
+    cli();
+    printf("Alignment Check Exception\n");
+    while(1);
+}
+
+void exception_MC() {
+    cli();
+    printf("Machine-Check Exception\n");
+    while(1);
+}
+
+void exception_XF() {
+    cli();
+    printf("SIMD Floating-Point Exception\n");
+    while(1);
 }
--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -1,1 +1,58 @@
+#ifndef IDT_H
+#define IDT_H
+
+#define DE      0       /*Divide Error Exception*/
+#define DB      1       /*Debug Exception*/
+#define NMI     2       /*NMI Interrupt*/
+#define BP      3       /*Breakpoint Exception*/
+#define OF      4       /*Overflow Exception*/
+#define BR      5       /*BOUND Range Exceeded Exception*/
+#define UD      6       /*Invalid Opcode Exception*/
+#define NM      7       /*Device Not Available Exception*/
+#define DF      8       /*Double Fault Exception*/
+#define CSO     9       /*Coprocessor Segment Overrun*/
+#define TS      10      /*Invalid TSS Exception*/
+#define NP      11      /*Segment Not Present*/
+#define SS      12      /*Stack Fault Exception*/
+#define GP      13      /*General Protection Exception*/
+#define PF      14      /*Page-Fault Exception*/
+#define MF      16      /*x87 FPU Floating-Point Error*/
+#define AC      17      /*Alignment Check Exception*/
+#define MC      18      /*Machine-Check Exception*/
+#define XF      19      /*SIMD Floating-Point Exception*/
+#define KB_INT  0x21    /*keyboard*/
+#define RTC_INT 0x28    /*real time clock*/
+#define SCV     0x80    /*system call vector*/
+#define INT_GATE    0   /*interrupt gate descriptor*/
+#define TRAP_GATE   1   /*trap gata descriptor*/
+#define CALL_GATE   2   /*call gate descriptor*/
+#define DPL_IE      0   /*Attempting  to  access  an  interrupt  or  exception
+                        handler  through  an  interrupt  or  trap  gate from
+                        virtual-8086 mode when the handler’s code segment DPL
+                        is greater than 0.*/
+#define DPL_SC      3   /*dpl of system call*/
+                        /*Gate descriptors are in themselves “gates,” which
+                        hold pointers to procedure entry points in code segments
+                        (call, interrupt, and trap gates) */
+
 void idt_init(unsigned index, unsigned gateType);
+void exception_DE();
+void exception_DB();
+void exception_NMI();
+void exception_BP();
+void exception_OF();
+void exception_BR();
+void exception_UD();
+void exception_NM();
+void exception_CSO();
+void exception_TS();
+void exception_NP();
+void exception_SS();
+void exception_GP();
+void exception_PF();
+void exception_MF();
+void exception_AC();
+void exception_MC();
+void exception_XF();
+
+#endif
