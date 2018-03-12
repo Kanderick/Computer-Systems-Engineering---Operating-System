fdd89df8851166454574f7a595458ca3583f41c8
Nickel Liang <zuodong2@illinois.edu>
2018-03-12 16:11:56 -0500

    Paging test added

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -1,82 +1,135 @@
 #include "paging.h"
 
-pde_t page_directory[PAGE_DIRECTORY_SIZE];
+/* Declaration of Global Page Directory/Table */
+pde_t page_directory[PAGE_DIRECTORY_SIZE] __attribute__((aligned (_4KB)));
 pte_t page_table_0[PAGE_TABLE_SIZE] __attribute__((aligned (_4KB)));
 
-static unsigned long read_cr0(void);
-// static unsigned long read_cr3(void);
-static unsigned long read_cr4(void);
-static void write_cr0(unsigned long val);
-static void write_cr3(unsigned long val);
-static void write_cr4(unsigned long val);
-static void set_in_cr0 (unsigned long mask);
-// static void clear_in_cr0 (unsigned long mask);
-static void set_in_cr4 (unsigned long mask);
-static void clear_in_cr4 (unsigned long mask);
+/* Static function definition */
+static inline unsigned long read_cr0(void);
+static inline unsigned long read_cr4(void);
+static inline void write_cr0(unsigned long val);
+static inline void write_cr3(unsigned long val);
+static inline void write_cr4(unsigned long val);
+static inline void set_in_cr0 (unsigned long mask);
+// static inline void clear_in_cr0 (unsigned long mask);
+static inline void set_in_cr4 (unsigned long mask);
+static inline void clear_in_cr4 (unsigned long mask);
 
-static unsigned long read_cr0(void) {
+/* read_cr0
+ * Purpose	Read in CR0 register value
+ * Inputs	None
+ * Outputs	CR0 value
+ * Side Effects None
+ */
+static inline unsigned long read_cr0(void) {
     unsigned long val;
     asm volatile("movl %%cr0,%0\n\t" :"=r" (val));
     return val;
 }
 
-// Does not use in this MP Checkpoint, reserved for future use
-// static unsigned long read_cr3(void) {
-//     unsigned long val;
-//     asm volatile("movl %%cr3,%0\n\t" :"=r" (val));
-//     return val;
-// }
-
-static unsigned long read_cr4(void) {
+/* read_cr4
+ * Purpose	Read in CR4 register value
+ * Inputs	None
+ * Outputs	CR4 value
+ * Side Effects None
+ */
+static inline unsigned long read_cr4(void) {
     unsigned long val;
     asm volatile("movl %%cr4,%0\n\t" :"=r" (val));
     return val;
 }
 
-static void write_cr0(unsigned long val) {
+/* write_cr0
+ * Purpose	Set CR0 register value
+ * Inputs	CR0 value
+ * Outputs	None
+ * Side Effects CR0 value changed
+ */
+static inline void write_cr0(unsigned long val) {
     asm volatile("movl %0,%%cr0": :"r" (val));
 }
 
-static void write_cr3(unsigned long val) {
+/* write_cr3
+ * Purpose	Set CR3 register value
+ * Inputs	CR3 value
+ * Outputs	None
+ * Side Effects CR3 value changed
+ */
+static inline void write_cr3(unsigned long val) {
     asm volatile("movl %0,%%cr3": :"r" (val));
 }
 
-static void write_cr4(unsigned long val) {
+/* write_cr4
+ * Purpose	Set CR4 register value
+ * Inputs	CR4 value
+ * Outputs	None
+ * Side Effects CR4 value changed
+ */
+static inline void write_cr4(unsigned long val) {
     asm volatile("movl %0,%%cr4": :"r" (val));
 }
 
-static void set_in_cr0(unsigned long mask) {
+/* set_in_cr0
+ * Purpose	Set CR0 register bit without change other bits
+ * Inputs	Mask for bits want to set
+ * Outputs	None
+ * Side Effects CR0 value changed
+ */
+static inline void set_in_cr0(unsigned long mask) {
     unsigned long cr0;
     cr0 = read_cr0();
     cr0 |= mask;
     write_cr0(cr0);
 }
 
-// Does not use in this MP Checkpoint, reserved for future use
-// static void clear_in_cr0(unsigned long mask) {
-//     unsigned long cr0;
-//     cr0 = read_cr0();
-//     cr0 &= ~mask;
-//     write_cr0(cr0);
-// }
+// Does not use in this CP3.1, reserved for future use
+/* clear_in_cr0
+ * Purpose	Clear CR0 register bit without change other bits
+ * Inputs	Mask for bits want to clear
+ * Outputs	None
+ * Side Effects CR0 value changed
+ */
+/*static inline void clear_in_cr0(unsigned long mask) {
+    unsigned long cr0;
+    cr0 = read_cr0();
+    cr0 &= ~mask;
+    write_cr0(cr0);
+}*/
 
-static void set_in_cr4(unsigned long mask) {
+/* set_in_cr4
+ * Purpose	Set CR4 register bit without change other bits
+ * Inputs	Mask for bits want to set
+ * Outputs	None
+ * Side Effects CR4 value changed
+ */
+static inline void set_in_cr4(unsigned long mask) {
 	unsigned long cr4;
 	cr4 = read_cr4();
 	cr4 |= mask;
 	write_cr4(cr4);
 }
 
-static void clear_in_cr4(unsigned long mask) {
+/* clear_in_cr4
+ * Purpose	Clear CR4 register bit without change other bits
+ * Inputs	Mask for bits want to clear
+ * Outputs	None
+ * Side Effects CR4 value changed
+ */
+static inline void clear_in_cr4(unsigned long mask) {
 	unsigned long cr4;
 	cr4 = read_cr4();
 	cr4 &= ~mask;
 	write_cr4(cr4);
 }
 
+/* init_paging
+ * Purpose	Turn on paging
+ * Inputs	None
+ * Outputs	None
+ * Side Effects CR0,3,4 value changed, paging turned on
+ */
 void init_paging(void) {
 	unsigned int i, j;
-
 	/* Flush page directory */
     for (i = 0; i < PAGE_DIRECTORY_SIZE; i++) {
         page_directory[i] = 0x00000000;
@@ -91,18 +144,18 @@ void init_paging(void) {
     page_directory[0] = ((unsigned long)(page_table_0) & PTBA_MASK) | PRESENT_MASK | R_W_MASK;
 
     /* Points to the start of Video memory */
-    page_table_0[0xB8] = VIDEO_START | R_W_MASK | PRESENT_MASK;
+    page_table_0[VIDEO_VIRTUAL] = VIDEO_START | R_W_MASK | PRESENT_MASK;
 
     /* Kernel 4MB page, present, R/W on */
     page_directory[1] = KERNEL_START | PAGE_SIZE_MASK | R_W_MASK | PRESENT_MASK;
 
     /* Set CR4 Flags */
-    clear_in_cr4(CR4_PAE_FLAG);
-	set_in_cr4(CR4_PSE_FLAG);
+    clear_in_cr4(CR4_PAE_FLAG); /* PAE set to 0 */
+	set_in_cr4(CR4_PSE_FLAG);   /* PSE set to 1 */
 
 	/* Set CR3 */
-    write_cr3((unsigned long)page_directory); // This instruction actullly flushed the tlb
+    write_cr3((unsigned long)page_directory);   /* This instruction flushed the tlb */
 
     /* Set CR0 bit 31*/
-    set_in_cr0(CR0_PG_FLAG);
+    set_in_cr0(CR0_PG_FLAG);    /* Assert Page Table is turned on */
 }
--- student-distrib/tests.h
+++ student-distrib/tests.h
@@ -1,13 +1,29 @@
 #ifndef TESTS_H
 #define TESTS_H
 
-#define PAGE_TEST 1
+/* General Test Macro */
+#define PASS 1
+#define FAIL 0
 
-// test launcher
+/* General Test Function */
 void launch_tests();
+
+/* Checkpoint 1 Tests Macro and Functions */
+#define PAGE_TEST               1           /* Set to 1 to run paging test */
+#define PAGE_TEST_ADDR_MASK     0x00000FFF  /* Mask out other flags */
+#define PAGE_TEST_UNPRESENT     0x00012000  /* Random page address */
+#define PAGE_TEST_KERNEL_ADDR   0x0040000F  /* Random address within kernel page */
+#define PAGE_TEST_VIDEO_ADDR    0x000B800F  /* Random address within video page */
+
 int idt_test();
+
 #if (PAGE_TEST == 1)
 int paging_test();
 #endif
+
+/* Checkpoint 2 tests */
+/* Checkpoint 3 tests */
+/* Checkpoint 4 tests */
+/* Checkpoint 5 tests */
 
 #endif /* TESTS_H */
