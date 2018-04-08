032dd098bc162bad04505183a9350a1f3b6bcac1
Nickel Liang <zuodong2@illinois.edu>
2018-04-07 20:54:10 -0500

    check validity

--- student-distrib/loader.c
+++ student-distrib/loader.c
@@ -3,6 +3,51 @@
 
 /* NOTE Limitation of this loader is that it assumes that and executable is always smaller than 4MB, which is specified in the MP3.3 documentation. However, this loader can be more flexible with a more complexed paging initialization strategy. */
 
+/* check_executable_validity
+ * Purpose Check if an file is a valid executable image.
+ * Inputs  filename User program file name.
+ * Outputs Error message when necessary.
+ * Return  -1   when file is not a valid executable
+ *         1    when file is a valid
+ * Side Effects None
+ * NOTE call before load_user_image
+ */
+int8_t check_executable_validity(const uint8_t* filename) {
+    /* Check if file opened */
+    int32_t fd = file_find(filename);
+    if (fd < 0) {
+        ERROR_MSG;
+        printf("File not opened.\n");
+        return 0;
+    }
+
+    /* Read first four bytes of the file */
+    int32_t nbytes = EXE_IDENTIFIER_SIZE;
+    uint8_t file_identifier[EXE_IDENTIFIER_SIZE];
+    int32_t read_bytes = file_read(fd, file_identifier, nbytes);
+    if (read_bytes == -1) {
+        ERROR_MSG;
+        printf("File read failed.\n");
+        return 0;
+    } else if (read_bytes != nbytes) {
+        ERROR_MSG;
+        printf("File read size does not match intended read size. Read %dB/%dB in file.\n", read_bytes, nbytes);
+        return 0;
+    }
+
+    /* Check if file is a valid executable */
+    uint8_t i;
+    for (i = 0; i < EXE_IDENTIFIER_SIZE; i++) {
+        if (file_identifier[i] != EXE_IDENTIFIER_TABLE[i]) {
+            ERROR_MSG;
+            printf("File identifier %d doesn't match. %u instead of %u.\n", i, file_identifier[i], EXE_IDENTIFIER_TABLE[i]);
+            printf("Not an executable image.\n");;
+            return 0;
+        }
+    }
+    return 1;
+}
+
  /* load_user_image
   * Purpose Load an user executable image into proper memory location.
   * Inputs  filename User program file name.
@@ -10,15 +55,11 @@
   * Return  Starting Address of the executable if valid,
   *         NULL if invalid or error occured.
   * Side Effects None
+  * NOTE call after check_executable_validity
   */
 uint32_t* load_user_image(const uint8_t* filename) {
-    /* Check if file exist */
-    int32_t fd = file_find(filename);
-    if (fd < 0) {
-        ERROR_MSG;
-        printf("File not opened.\n");
-        return NULL;
-    }
+    /* Get fd number */
+    int32_t fd = file_find(filename);   // File open check is not necessary since called check_executable_validity before.
 
     /* Get file length, check if it exceed maximum allowable image size */
     int32_t nbytes = get_file_length(filename);
@@ -43,18 +84,6 @@ uint32_t* load_user_image(const uint8_t* filename) {
         WARNING_MSG;
         printf("File read size does not match file size. Read %dB/%dB in file.\n", read_bytes, nbytes);
         // WARNING Function not returned here. Change to ERROR_MSG and return if necessary.
-    }
-
-    /* Check if file is a valid executable */
-    uint8_t* file_identifier = (uint8_t*)LOAD_EXE_START_ADDR;
-    uint8_t i;
-    for (i = 0; i < EXE_IDENTIFIER_SIZE; i++) {
-        if (file_identifier[i] != EXE_IDENTIFIER_TABLE[i]) {
-            ERROR_MSG;
-            printf("File identifier %d doesn't match. %u instead of %u.\n", i, file_identifier[i], EXE_IDENTIFIER_TABLE[i]);
-            printf("Not an executable image.\n");;
-            return NULL;
-        }
     }
 
     /* Get program starting address */
