a94c6aae5e4ce0d47d4539787143121a2bfb2ef1
Xie <fengzex2@illinois.edu>
2018-04-08 18:08:06 -0500

    change gate type

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -20,29 +20,29 @@ void idt_init_exceptions(void){
     for (i = 0; i < 256; i ++) {            /*fill all the entry in the idt table with unkown interrupt*/
         idt_init(i, INT_GATE, &unkown_int_wrapper);
     }
-    idt_init(DE, TRAP_GATE, &exception_DE_wrapper);         /*fill the exception part with each specific instruction*/
-    idt_init(DB, TRAP_GATE, &exception_DB_wrapper);
+    idt_init(DE, INT_GATE, &exception_DE_wrapper);         /*fill the exception part with each specific instruction*/
+    idt_init(DB, INT_GATE, &exception_DB_wrapper);
     idt_init(NMI, INT_GATE, &exception_NMI_wrapper);
-    idt_init(BP, TRAP_GATE, &exception_BP_wrapper);
-    idt_init(OF, TRAP_GATE, &exception_OF_wrapper);
-    idt_init(BR, TRAP_GATE, &exception_BR_wrapper);
-    idt_init(UD, TRAP_GATE, &exception_UD_wrapper);
-    idt_init(NM, TRAP_GATE, &exception_NM_wrapper);
-    idt_init(DF, TRAP_GATE, &exception_DF_wrapper);
-    idt_init(CSO, TRAP_GATE, &exception_CSO_wrapper);
-    idt_init(TS, TRAP_GATE, &exception_TS_wrapper);
-    idt_init(NP, TRAP_GATE, &exception_NP_wrapper);
-    idt_init(SS, TRAP_GATE, &exception_SS_wrapper);
-    idt_init(GP, TRAP_GATE, &exception_GP_wrapper);
-    idt_init(PF, TRAP_GATE, &exception_PF_wrapper);
-    idt_init(UN, TRAP_GATE, &unkown_int_wrapper);
-    idt_init(MF, TRAP_GATE, &exception_MF_wrapper);
-    idt_init(AC, TRAP_GATE, &exception_AC_wrapper);
-    idt_init(MC, TRAP_GATE, &exception_MC_wrapper);
-    idt_init(XF, TRAP_GATE, &exception_XF_wrapper);
+    idt_init(BP, INT_GATE, &exception_BP_wrapper);
+    idt_init(OF, INT_GATE, &exception_OF_wrapper);
+    idt_init(BR, INT_GATE, &exception_BR_wrapper);
+    idt_init(UD, INT_GATE, &exception_UD_wrapper);
+    idt_init(NM, INT_GATE, &exception_NM_wrapper);
+    idt_init(DF, INT_GATE, &exception_DF_wrapper);
+    idt_init(CSO, INT_GATE, &exception_CSO_wrapper);
+    idt_init(TS, INT_GATE, &exception_TS_wrapper);
+    idt_init(NP, INT_GATE, &exception_NP_wrapper);
+    idt_init(SS, INT_GATE, &exception_SS_wrapper);
+    idt_init(GP, INT_GATE, &exception_GP_wrapper);
+    idt_init(PF, INT_GATE, &exception_PF_wrapper);
+    idt_init(UN, INT_GATE, &unkown_int_wrapper);
+    idt_init(MF, INT_GATE, &exception_MF_wrapper);
+    idt_init(AC, INT_GATE, &exception_AC_wrapper);
+    idt_init(MC, INT_GATE, &exception_MC_wrapper);
+    idt_init(XF, INT_GATE, &exception_XF_wrapper);
     idt_init(KB_INT, INT_GATE, &keyboard_wrapper);      /*fill the keyboard interruption entry*/
     idt_init(RTC_INT, INT_GATE, &rtc_wrapper);          /*fill the rtc interruption entry*/
-    idt_init(SCV, CALL_GATE, &system_call_wrapper);
+    idt_init(SCV, TRAP_GATE, &system_call_wrapper);
     // init rtc flag here
     rtcFlag = 1;
 }
@@ -82,7 +82,7 @@ void idt_init(unsigned index, unsigned gateType, void *handler) {
             desc.reserved2 = 1;
             desc.reserved1 = 1;
             desc.reserved0 = 0;
-            desc.dpl = DPL_IE;
+            desc.dpl = DPL_SC;
             break;
         case CALL_GATE:
             desc.reserved4 = 0;
@@ -90,10 +90,6 @@ void idt_init(unsigned index, unsigned gateType, void *handler) {
             desc.reserved2 = 0;
             desc.reserved1 = 1;
             desc.reserved0 = 0;
-            switch(index) {
-                case SCV:
-                    desc.dpl = DPL_SC;
-            }
             break;
     }
     idt[index] = desc;          /*put back the entry to the idt table*/
@@ -112,7 +108,6 @@ void idt_init(unsigned index, unsigned gateType, void *handler) {
 void exception_DE(void) {
     cli();          /*clean the interrupt flag*/
     printf("Divide Error Exception\n");
-    while(1);           /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -128,7 +123,6 @@ void exception_DE(void) {
 void exception_DB(void) {
      cli();         /*clean the interrupt flag*/
     printf("Debug Exception\n");
-     while(1);          /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -144,7 +138,6 @@ void exception_DB(void) {
 void exception_NMI(void) {
      cli();         /*clean the interrupt flag*/
     printf("NMI Interrupt\n");
-     while(1);          /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -160,7 +153,6 @@ void exception_NMI(void) {
 void exception_BP(void) {
      cli();         /*clean the interrupt flag*/
     printf("Breakpoint Exception\n");
-     while(1);          /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -176,7 +168,6 @@ void exception_BP(void) {
 void exception_OF(void) {
      cli();         /*clean the interrupt flag*/
     printf("Overflow Exception\n");
-     while(1);          /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -192,7 +183,6 @@ void exception_OF(void) {
 void exception_BR(void) {
      cli();         /*clean the interrupt flag*/
     printf("BOUND Range Exceeded Exception\n");
-     while(1);          /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -208,7 +198,6 @@ void exception_BR(void) {
 void exception_UD(void) {
      cli();         /*clean the interrupt flag*/
     printf("Invalid Opcode Exception\n");
-     while(1);          /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -224,7 +213,6 @@ void exception_UD(void) {
 void exception_NM(void) {
      cli();         /*clean the interrupt flag*/
     printf("Device Not Available Exception\n");
-     while(1);          /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -240,7 +228,6 @@ void exception_NM(void) {
 void exception_DF(void) {
      cli();         /*clean the interrupt flag*/
     printf("Double Fault Exception\n");
-     while(1);          /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -256,7 +243,6 @@ void exception_DF(void) {
 void exception_CSO(void) {
      cli();         /*clean the interrupt flag*/
     printf("Coprocessor Segment Overrun\n");
-     while(1);          /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -272,7 +258,6 @@ void exception_CSO(void) {
 void exception_TS(void) {
      cli();         /*clean the interrupt flag*/
     printf("Invalid TSS Exception\n");
-     while(1);          /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -288,7 +273,6 @@ void exception_TS(void) {
 void exception_NP(void) {
     cli();          /*clean the interrupt flag*/
     printf("Segment Not Present\n");
-    while(1);           /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -304,7 +288,6 @@ void exception_NP(void) {
 void exception_SS(void) {
     cli();          /*clean the interrupt flag*/
     printf("Stack Fault Exception\n");
-    while(1);           /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -320,7 +303,6 @@ void exception_SS(void) {
 void exception_GP(void) {
     cli();          /*clean the interrupt flag*/
     printf("General Protection Exception\n");
-    while(1);           /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -336,7 +318,6 @@ void exception_GP(void) {
 void exception_PF(void) {
     cli();          /*clean the interrupt flag*/
     printf("Page-Fault Exception\n");
-    while(1);           /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -352,7 +333,6 @@ void exception_PF(void) {
 void exception_MF(void) {
     cli();          /*clean the interrupt flag*/
     printf("x87 FPU Floating-Point Error\n");
-    while(1);           /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -368,7 +348,6 @@ void exception_MF(void) {
 void exception_AC(void) {
     cli();          /*clean the interrupt flag*/
     printf("Alignment Check Exception\n");
-    while(1);           /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -384,7 +363,6 @@ void exception_AC(void) {
 void exception_MC(void) {
     cli();          /*clean the interrupt flag*/
     printf("Machine-Check Exception\n");
-    while(1);           /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -400,7 +378,6 @@ void exception_MC(void) {
 void exception_XF(void) {
     cli();          /*clean the interrupt flag*/
     printf("SIMD Floating-Point Exception\n");
-    while(1);       /*because it is a exception, freeze the program*/
 }
 
 /*
@@ -416,5 +393,4 @@ void exception_XF(void) {
 void unkown_int(void) {
     cli();          /*clean the interrupt flag*/
     printf("unkown interruption\n");
-    while(1);           /*because it is a exception, freeze the program*/
 }
