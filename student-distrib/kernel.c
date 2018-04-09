ad9ddc4159641adb51a7f9409d7b062d03c5cb90
unknown <fengzex2@illinois.edu>
2018-04-09 17:38:11 -0500

    reset shell

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -180,7 +180,10 @@ void entry(unsigned long magic, unsigned long addr) {
     launch_tests();
 #endif
     /* Execute the first program ("shell") ... */
-    while(1) execute((void *)"shell");
+    clearScreen();
+    while(1) {
+        execute((void *)"shell");
+    }
     /* Spin (nicely, so we don't chew up cycles) */
     asm volatile (".1: hlt; jmp .1;");
 }
